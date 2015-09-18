3a774b875867f4be283b51c1bfa583b3d8f33bcc
Edwin Zhang <ehzhang@mit.edu>
2015-09-18 18:49:46 -0400

    yolo styling

--- js/load_facebook.js
+++ js/load_facebook.js
@@ -28,40 +28,41 @@ function facebookLogin() {
 }
 
 function loadFBProfileImage(fbid) {
-  var loadeds = [false, false]
+  var loadeds = [false, false];
 	var canvas = $('#fb-img').get(0);
 	var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.globalCompositeOperation = "soft-light"
+  ctx.globalCompositeOperation = "soft-light";
   var image = new Image();
-  image.crossOrigin = "Anonymous"
-  image.src = "http://graph.facebook.com/" + fbid + "/picture?width=500&height=500";
+  image.crossOrigin = "Anonymous";
+  image.src = "http://graph.facebook.com/" + fbid + "/picture?width=320&height=320";
   image.onload = function() {
+    $('#placeholder').fadeOut();
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#362487";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     if (loadeds[1]) {
       var img = canvas.toDataURL('image/png');
-      $("#download").attr("href", img)
+      $("#download").attr("href", img);
     }
     else {
       loadeds[0] = true;
     }
-  }
+  };
   var newimage = new Image();
   newimage.crossOrigin = "Anonymous";
-  newimage.src = 'hackoverlay.png'
+  newimage.src = 'hackoverlay.png';
   newimage.onload = function () {
-    ctx.globalCompositeOperation = "overlay"
-    ctx.drawImage(newimage, 0, 0, canvas.width, canvas.height)
+    ctx.globalCompositeOperation = "overlay";
+    ctx.drawImage(newimage, 0, 0, canvas.width, canvas.height);
     if (loadeds[0]) {
       var img = canvas.toDataURL('image/png');
-      $("#download").attr("href", img)
+      $("#download").attr("href", img);
     }
     else {
       loadeds[1] = true;
     }
-  }
+  };
 }
 /*
 function processNewImage() {
