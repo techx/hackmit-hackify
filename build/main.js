54597b403cf28148f9d5bff73d530931a1d7674e
Ajay Jain <ajayjain@mit.edu>
2017-08-20 23:56:18 -0700

    Webcam + faction toggle

--- build/main.js
+++ build/main.js
@@ -2,6 +2,106 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var facebookInit = function () {
+    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        var render, profilePicture, color, logo;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        render = new Rendering();
+                        _context.next = 3;
+                        return ImageLoader.fromFacebook();
+
+                    case 3:
+                        profilePicture = _context.sent;
+
+                        render.drawImage(profilePicture);
+
+                        color = $("input[name=faction]:checked").val();
+
+                        // const cornerRect = Rect.fromPercents(render.canvas.width, render.canvas.height, 0.75, 0.75, 0.2, 0.2);
+
+                        _context.next = 8;
+                        return ImageLoader.fromURL("assets/hackoverlay.png");
+
+                    case 8:
+                        logo = _context.sent;
+
+                        render.drawImage(logo, { globalCompositeOperation: "overlay" });
+                        render.fillRect({ color: color, globalCompositeOperation: "color" });
+                        // render.fillRect({color: "#1897ea", globalCompositeOperation: "color"});
+                        // render.fillRect({color: "#FF585D", globalCompositeOperation: "color"});
+
+                        $("#download").attr("href", render.toDataURL());
+                        $('#placeholder').fadeOut();
+                        $('#load-buttons').fadeOut();
+
+                        // Rerun function if the user toggles their faction
+                        $("input[name=faction]").off("change").change(facebookInit);
+
+                    case 15:
+                    case "end":
+                        return _context.stop();
+                }
+            }
+        }, _callee, this);
+    }));
+
+    return function facebookInit() {
+        return _ref3.apply(this, arguments);
+    };
+}();
+
+var webcamInit = function () {
+    var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
+        var video, logo, render, frameLoop;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+            while (1) {
+                switch (_context2.prev = _context2.next) {
+                    case 0:
+                        frameLoop = function frameLoop() {
+                            var color = $("input[name=faction]:checked").val();
+
+                            render.clear();
+                            render.drawImage(video);
+                            render.drawImage(logo, { globalCompositeOperation: "overlay" });
+                            render.fillRect({ color: color, globalCompositeOperation: "color" });
+
+                            $("#download").attr("href", render.toDataURL());
+                            requestAnimationFrame(frameLoop);
+                        };
+
+                        _context2.next = 3;
+                        return ImageLoader.fromWebcam();
+
+                    case 3:
+                        video = _context2.sent;
+                        _context2.next = 6;
+                        return ImageLoader.fromURL("assets/hackoverlay.png");
+
+                    case 6:
+                        logo = _context2.sent;
+                        render = new Rendering();
+
+
+                        requestAnimationFrame(frameLoop);
+                        $('#placeholder').fadeOut();
+                        $('#load-buttons').fadeOut();
+
+                    case 11:
+                    case "end":
+                        return _context2.stop();
+                }
+            }
+        }, _callee2, this);
+    }));
+
+    return function webcamInit() {
+        return _ref4.apply(this, arguments);
+    };
+}();
+
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -38,6 +138,23 @@ var ImageLoader = function () {
                         console.log("User cancelled login or did not fully authorize.");
                         reject("User cancelled login or did not fully authorize.");
                     }
+                });
+            });
+        }
+    }, {
+        key: "fromWebcam",
+        value: function fromWebcam() {
+            return new Promise(function (resolve, reject) {
+                navigator.getUserMedia({ video: true, audio: false }, function (stream) {
+                    var video = document.createElement('video');
+                    video.setAttribute("autoplay", true);
+                    video.src = window.URL.createObjectURL(stream);
+                    video.addEventListener('loadedmetadata', function () {
+                        return resolve(video);
+                    });
+                }, function (e) {
+                    console.error('Webcam permissions request rejected!', e);
+                    reject(e);
                 });
             });
         }
@@ -92,6 +209,7 @@ var Rendering = function () {
     _createClass(Rendering, [{
         key: "clear",
         value: function clear() {
+            this.ctx.globalCompositeOperation = "source-over";
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         }
     }, {
@@ -143,45 +261,6 @@ var Rendering = function () {
     return Rendering;
 }();
 
-$("#facebook-init").click(function () {
-    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(el) {
-        var render, profilePicture, logo;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-                switch (_context.prev = _context.next) {
-                    case 0:
-                        render = new Rendering();
-                        _context.next = 3;
-                        return ImageLoader.fromFacebook();
-
-                    case 3:
-                        profilePicture = _context.sent;
-
-                        render.drawImage(profilePicture);
-
-                        _context.next = 7;
-                        return ImageLoader.fromURL("assets/hackoverlay.png");
-
-                    case 7:
-                        logo = _context.sent;
-
-                        // const cornerRect = Rect.fromPercents(render.canvas.width, render.canvas.height, 0.75, 0.75, 0.2, 0.2);
-                        render.drawImage(logo, { globalCompositeOperation: "overlay" });
-                        render.fillRect({ globalCompositeOperation: "soft-light" });
-
-                        $("#download").attr("href", render.toDataURL());
-                        $('#placeholder').fadeOut();
-
-                    case 12:
-                    case "end":
-                        return _context.stop();
-                }
-            }
-        }, _callee, undefined);
-    }));
-
-    return function (_x3) {
-        return _ref3.apply(this, arguments);
-    };
-}());
+$("#facebook-init").click(facebookInit);
+$('#webcam-init').click(webcamInit);
 //# sourceMappingURL=main.js.map
